c9cdcae9b39364fc1dc052f3bd64a696f51193c4
MohammedHRashid <109778287+MohammedHRashid@users.noreply.github.com>
2025-03-12 14:10:18 +0000

    Update main.py

--- starter/main.py
+++ starter/main.py
@@ -81,6 +81,7 @@ def make_predictions(data: List[Data]) -> dict:
 
     # Process input data using same encoding as training data
     input_data, _, _, _ = process_data(df, categorical_features, encoder=encoder, lb=lb, training=False)
+    print(input_data)
 
     # Make inference predictions
     preds = inference(model, input_data)
